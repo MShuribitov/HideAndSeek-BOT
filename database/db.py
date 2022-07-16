51a604cddd5423edb07c30b9aee04454da6ac8c9
Magomed Shuribitov <maga785@bk.ru>
2022-07-16 19:10:12 +0300

    Joins

--- database/db.py
+++ database/db.py
@@ -1,9 +1,7 @@
 import sqlite3
 
-class CreateTable:
-    def __init__(self, userid, username, chatid):
-        self.userid = str(userid)
-        self.username = username
+class DataBase:
+    def __init__(self, chatid):
         db = sqlite3.connect('database/groups.db')
         cursor = db.cursor()
         cursor.execute(f"""CREATE TABLE IF NOT EXISTS group_{chatid} (
@@ -12,6 +10,12 @@ class CreateTable:
             u_role TEXT,
             u_position TEXT
         )""")
+    def adduser(self, chatid, userid, username):
+        db = sqlite3.connect('database/groups.db')
+        cursor = db.cursor()
+        self.chatid = chatid
+        self.userid = userid
+        self.username = username
         cursor.execute(f"SELECT u_id FROM group_{chatid} WHERE u_id = {self.userid}")
         check = cursor.fetchall()
         if not check:
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 from telebot import types
-from database.db import *
+from database.db import DataBase
 
 TOKEN = "5522146184:AAFM2c2k7OIsJSBuAH2ibBhMFQFDJsSGHhM"
 bot = telebot.TeleBot(TOKEN)
@@ -10,7 +10,16 @@ def start(message):
     bot.send_message(message.chat.id, f"Привет, {message.from_user.username}")
 @bot.message_handler(commands=['game'])
 def start_game(message):
-    CreateTable(message.from_user.id, message.from_user.username, message.chat.id)
-    bot.send_message(message.chat.id, "Игра успешно началась")
+    DataBase(message.chat.id)
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    join = types.InlineKeyboardButton("✅ Войти", callback_data="join")
+    markup.add(join)
+    bot.send_message(message.chat.id, "⚠️ Начался набор игроков", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.message:
+        if call.data == "join":
+            DataBase.adduser(call.message.chat.id, call.from_user.id, call.from_user.username)
 
 bot.polling(none_stop=True)
